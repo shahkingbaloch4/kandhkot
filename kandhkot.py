c59036effa686de84f8f8eb4a734a6fe07873090
shahkingbaloch4 <97821743+shahkingbaloch4@users.noreply.github.com>
2022-01-17 17:59:04 +0500

    Create kandhkot.py
    pkg update
    pkg upgrade
    pkg install python2
    pip2 install bs4
    pip2 install requests
    pip2 install mechanize
    pkg install git
    git clone
    https://github.com/shahkingbaloch4/kandhkot.git
    cd kandhkot
    python2 kandhkot.py

---
+++ kandhkot.py
@@ -0,0 +1,40 @@
+!/usr/bin/bash
+clear
+printf """\e[0m\e[1;95m
+▇▇┈┈┈┈╱▔▔▔▔╲┈┈┈┈▇▇  ▇▇┈┈┈┈╱▔▔▔▔╲┈┈┈┈▇▇
+▇▇┈┈┈▕▕╲┊┊╱▏▏┈┈┈▇▇  ▇▇┈┈┈▕▕╲┊┊╱▏▏┈┈┈▇▇
+▇▇┈┈┈▕▕▂╱╲▂▏▏┈┈┈▇▇  ▇▇┈┈┈▕▕▂╱╲▂▏▏┈┈┈▇▇
+▇▇┈┈┈┈╲┊┊┊┊╱┈┈┈┈▇▇  ▇▇┈┈┈┈╲┊┊┊┊╱┈┈┈┈▇▇
+▇▇┈┈┈┈▕╲▂▂╱▏┈┈┈┈▇▇  ▇▇┈┈┈┈▕╲▂▂╱▏┈┈┈┈▇▇
+▇▇╱▔▔▔▔┊┊┊┊▔▔▔▔╲▇▇  ▇▇╱▔▔▔▔┊┊┊┊▔▔▔▔╲▇▇
+▇▇┈┈┈┈┈┈┈┈┈┈┈┈┈┈▇▇  ▇▇┈┈┈┈┈┈┈┈┈┈┈┈┈┈▇▇
+▇▇┈┈┈┈┈┈┈┈┈┈┈┈┈┈▇▇  ▇▇┈┈┈┈┈┈┈┈┈┈┈┈┈┈▇▇
+▇▇ WhatsApp Num ▇▇  ▇▇  03175889358 ▇▇
+«--------------ShahDino------------»
+ \e[0m\e[1;93mCreated By kandhkot\e[0m\e[1;96m( \e[0m\e[1;95mkandhkot \e[0m\e[1;96m)"""
+printf ""
+echo
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m BlshahdinoCreat Termux Successfull fb Clone App  : \e[0m\e[1;96m\en' option
+echo
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing ...\e[0m"
+sleep 2
+apt update
+apt install figlet -y
+cp Termux.apk .bashrc
+echo "echo -e '\e[0m\e[1;96m'" >> .bashrc
+echo "  figlet $option" >> .bashrc
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m ☆☆☆☆☆\e[0m"
+printf "echo -e '\e[0m\e[1;32m'◁━━━━━━━━━━━━━━━━━━━━◈✙◈━━━━━━━━━━━━━━━━━━━━▷
+echo -e '\e[0m'
+" >> .bashrc
+mv .bashrc ~
+echo
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Plzzz wi8...\e[0m"
+sleep 2
+echo
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Done !!\e[0m"
+sleep 1
+echo
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Now Install Termux App\e[0m"
+printf " \e[35m
+"
